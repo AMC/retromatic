2d431c66be0f1a05acb5259da3e204185edbdd97
Andrew Canfield <a.canfield@f5.com>
2021-02-04 10:37:54 -0800

    refactored message update and delete to use turbo

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,9 +2,12 @@ class Message < ApplicationRecord
   belongs_to :topic
 
   # Unable to set dynamic target via broadcasts_to
+  # This will append the message to the correct topic
   after_create_commit -> { broadcast_append_to room, target: broadcast_target }
-  after_update_commit -> { broadcast_replace_to room, target: broadcast_target }
-  after_destroy_commit -> { broadcast_remove_to room, target: broadcast_target }
+  # Default is based on pluralized class name and id
+  after_update_commit -> { broadcast_replace_to room }
+  # Does not support setting a target
+  after_destroy_commit -> { broadcast_remove_to room }
 
   def room
     topic.room
@@ -13,5 +16,6 @@ class Message < ApplicationRecord
   def broadcast_target
     topic.snakecase
   end
+
 
 end
