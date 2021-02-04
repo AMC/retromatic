993bdafbb91347c185fd4c9096cc9a0ff3c6ea1f
Andrew Canfield <a.canfield@f5.com>
2021-02-04 10:13:15 -0800

    Refactored create message to use turbo

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def snakecase
+    self.name.gsub(/( )/, '_').downcase
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,17 @@
 class Message < ApplicationRecord
   belongs_to :topic
+
+  # Unable to set dynamic target via broadcasts_to
+  after_create_commit -> { broadcast_append_to room, target: broadcast_target }
+  after_update_commit -> { broadcast_replace_to room, target: broadcast_target }
+  after_destroy_commit -> { broadcast_remove_to room, target: broadcast_target }
+
+  def room
+    topic.room
+  end
+
+  def broadcast_target
+    topic.snakecase
+  end
+
 end
